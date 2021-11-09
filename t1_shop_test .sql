2498c7bb92fa68d9684746a561bc9da9ee09b26f
oharastudent2002067 <93752973+oharastudent2002067@users.noreply.github.com>
2021-11-09 15:05:10 +0900

    test_ uploadsqlfile

---
+++ t1_shop_test .sql
@@ -0,0 +1,11 @@
+CREATE TABLE t1_shop_test (
+    t1_shop_id VARCHAR(10) PRIMARY KEY,
+    t1_shop_name_prime  varchar(30) NOT NULL,
+    t10_area_id  varchar(6),
+    t1_addres TEXT,
+    
+
+
+
+
+)
---
+++ test_tables.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE test_tables (
+
+-- name VARCHAR(20),
+
+-- age INTEGER
+
+-- );
+INSERT INTO test_tables VALUES('test001',112);
+
+select * from test_tables;
